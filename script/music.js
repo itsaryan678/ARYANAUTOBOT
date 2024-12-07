7de1b4f372a87406bb32399baee15c6ccbd41fa0
Itz Aryan <163823860+itsaryan678@users.noreply.github.com>
2024-12-07 21:04:04 +0530

    Update music.js

--- script/music.js
+++ script/music.js
@@ -14,9 +14,6 @@ async function downloadFile(url, filePath) {
         url,
         method: 'GET',
         responseType: 'stream',
-        headers: {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-        }
     });
     response.data.pipe(writer);
     return new Promise((resolve, reject) => {
@@ -37,8 +34,7 @@ module.exports = {
         credits: 'Team Clayx | Developer',
         cooldown: 5
     },
-
-    onStart: async function({ api, message, event, args }) {
+    run: async function({ api, event, args }) {
         const query = args.join(' ');
 
         if (!query) {
